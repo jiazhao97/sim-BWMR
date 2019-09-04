05d4f446100c843b3e0061b1cb39a77260524386
jiazhao97 <45305585+jiazhao97@users.noreply.github.com>
2019-09-04 21:47:39 +0800

    Update sim-summary-case3-typeIerror-power.R

--- sim-summary-case3-typeIerror-power.R
+++ sim-summary-case3-typeIerror-power.R
@@ -1,6 +1,5 @@
 ##### Case-3 in summary-level simulations (qq-plot, type I error rate, power) ####
-# Figures S13 in Supplementary Document
-
+# Figures S11 in Supplementary Document
 
 rm(list = ls())
 
@@ -161,4 +160,3 @@ my.pvalue.list <- list("GSMR" = pval.gsmr[1, ][pval.gsmr[1, ] > 1e-8], "RAPS" = 
                        "Egger" = pval.egger[1, ][pval.egger[1, ] > 1e-8], "BWMR" = pval.bwmr[1, ][pval.bwmr[1, ] > 1e-8])
 plt_qq <- qqunif.plot(my.pvalue.list, auto.key=list(corner=c(.95,.05)))
 plt_qq
-
